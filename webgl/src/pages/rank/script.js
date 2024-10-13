1a67a4d62d159927c4ab6b4af5a130e69a9836de
llddang <bodyness@naver.com>
2024-10-13 14:46:44 +0900

    feat: 랭킹 점수 표기 변경

--- webgl/src/pages/rank/script.js
+++ webgl/src/pages/rank/script.js
@@ -1,26 +1,46 @@
 axios({
-  method: 'get',
-  url: 'http://172.171.134.142:8080/rankings'
-}).then((res) => {
-  if(res.status !== 200) return;
-  
-  console.log(res.data);
-  const tbody = document.getElementById("table-content");
-  res.data.map((data) => {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `<td style="width: 50px;">${data.rankNumber}</td> <td style="width: 150px;">${data.nickName}</td> <td style="width: 100px;">${data.maxScore}</td>`
+  method: "get",
+  url: "http://172.171.134.142:8080/rankings",
+})
+  .then((res) => {
+    if (res.status !== 200) return;
 
-    tbody.append(tr);
+    console.log(res.data);
+    const tbody = document.getElementById("table-content");
+    res.data.map((data) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `<td style="width: 50px;">${
+        data.rankNumber
+      }</td> <td style="width: 150px;">${
+        data.nickName
+      }</td> <td style="width: 100px;">${timerFormat(data.maxScore)}</td>`;
+
+      tbody.append(tr);
+    });
   })
-  
-}).catch((err) => {
-  const tostMessage = document.getElementById("tost-message");
-  tostMessage.innerText = "랭킹을 불러오는데 실패했습니다.!";
-  tostMessage.style.background = "#ff000070";
-  tostMessage.classList.add('active');
+  .catch((err) => {
+    const tostMessage = document.getElementById("tost-message");
+    tostMessage.innerText = "랭킹을 불러오는데 실패했습니다.!";
+    tostMessage.style.background = "#ff000070";
+    tostMessage.classList.add("active");
 
-  setTimeout(function(){
-    tostMessage.classList.remove('active');
-    location.replace("/");
-  },1000);
-});
+    setTimeout(function () {
+      tostMessage.classList.remove("active");
+      location.replace("/");
+    }, 1000);
+  });
+
+function timerFormat(time) {
+  const mm = parseInt(time / (60 * 100));
+  time %= 60 * 100;
+  const ss = parseInt(time / 100);
+  time %= 100;
+
+  return `${pad0(mm, 2)}:${pad0(ss, 2)}:${pad0(time, 2)}`;
+}
+
+function pad0(value, count) {
+  var result = value.toString();
+  for (; result.length < count; --count) result = "0" + result;
+  return result;
+}
